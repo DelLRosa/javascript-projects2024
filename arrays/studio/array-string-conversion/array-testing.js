1a96875f5666c7bdd2c541b6b3dd8aa8988ebd3f
Delaney Green <delaney.rosa1@gmail.com>
2024-03-28 14:12:08 -0500

    studio solutions

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -1,3 +1,4 @@
+
 let protoArray1 = "3,6,9,12";
 let protoArray2 = "A;C;M;E";
 let protoArray3 = "space delimited string";
@@ -5,48 +6,53 @@ let protoArray4 = "Comma-spaces, might, require, typing, caution";
 
 strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 
-//2) 
+//b) 
 function reverseCommas() {
-	//TODO: 1. create and instantiate your variables.
-	let check; // bracket notation to access a specific item in the strings array
-	let output; // initialize to empty array, will hold the result of your string/array manipulation. 
-
-	//code goes here
-
-
-	//TODO: 2. write the code required for this step
+	//TODO: create and instantiate your variables.
+	let check = strings[0];
+	let output = [];
+	//TODO: write the code required for this step
+	if(check.includes(',')) {
+		output = check.split(',').reverse().join(',')
+	}
 
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
 	return output;
 }
 
-//3)
+//c)
 function semiDash() {
-	let check;
-	let output;
-//TODO: write the code required for this step
+	let check = strings[1];
+	let output = [];
 
-  
+	if(check.includes(';')) {
+		output = check.split(';').sort().join('-');
+		} 
 	return output;
 }
 
-//4)
+//d)
 function reverseSpaces() {
-	let check;
-	let output;
-  //TODO: write the code required for this step
+	let check = strings[2]
+	let output = [];
 
+	if(check.includes(' ')) {
+		output = check.split(' ').sort().reverse().join(' ');
+	}
 	return output;
 }
 
-//5)
+//e)
 function commaSpace() {
-	let check;
-	let output;
-	//TODO: write the code required for this step
-  
+	let check = strings[3];
+	let output = [];
+
+	if(check.includes(', ')) {
+		output = check.split(', ').reverse().join(',');
+	}
 	return output;
 }
+
 
 // NOTE: Don't add or modify any code below this line or your program might not run as expected.
 module.exports = {
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,23 +1,67 @@
-const input = require('readline-sync');
-// let str = "LaunchCode";
-let str= input.question("enter a string: ");
-let numChar= input.question("enter the number of characters to move: ")
 
-if (numChar>str.length){
-    numChar = 3;
-    let newStr = str.slice(numChar) + str.slice(0,numChar);
-    console.log(`Too long! Defaulted to 3 characters. The old string is ${str}, the new string is ${newStr}`);
-} else {
-    let newStr = str.slice(numChar) + str.slice(0,numChar);
-    console.log(`The old string is ${str} and the new string is ${newStr}.`);
-}
+const input = require('readline-sync');
+let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-let newStr = str.slice(numChar) + str.slice(0,numChar);
-// console.log(newStr);
+
 //Use a template literal to print the original and modified string in a descriptive phrase.
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const input = require('readline-sync');
+// // let str = "LaunchCode";
+// let str= input.question("enter a string: ");
+// let numChar= input.question("enter the number of characters to move: ")
+
+// if (numChar>str.length){
+//     numChar = 3;
+//     let newStr = str.slice(numChar) + str.slice(0,numChar);
+//     console.log(`Too long! Defaulted to 3 characters. The old string is ${str}, the new string is ${newStr}`);
+// } else {
+//     let newStr = str.slice(numChar) + str.slice(0,numChar);
+//     console.log(`The old string is ${str} and the new string is ${newStr}.`);
+// }
+
+// //1) Use string methods to remove the first three characters from the string and add them to the end.
+// //Hint - define another variable to hold the new string or reassign the new string to str.
+// let newStr = str.slice(numChar) + str.slice(0,numChar);
+// // console.log(newStr);
+// //Use a template literal to print the original and modified string in a descriptive phrase.
+
+// //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+
+// //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
